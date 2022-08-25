623e05458a4704008ec10b9078e474c28a4b9572
Jeremy New <93681678+jayynew94@users.noreply.github.com>
2022-08-25 17:43:18 -0500

    Add user authentication Express middleware

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -44,50 +44,52 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
 
-  User.init({
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [4, 30],
-        isNotEmail(value) {
-          if (Validator.isEmail(value)) {
-            throw new Error("Cannot be an email.");
-          }
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+        },
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
         },
       },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 256],
-        isEmail: true,
-      },
-    },
-    hashedPassword: {
-      type: DataTypes.STRING.BINARY,
-      allowNull: false,
-      validate: {
-        len: [60, 60],
-      },
-    },
-
+    {
       sequelize,
       modelName: "User",
       defaultScope: {
         attributes: {
-          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
-        }
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
+        },
       },
       scopes: {
         currentUser: {
-          attributes: { exclude: ["hashedPassword"] }
+          attributes: { exclude: ["hashedPassword"] },
         },
         loginUser: {
-          attributes: {}
-        }
-      }
-  });
+          attributes: {},
+        },
+      },
+    }
+  );
   return User;
 };
--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,12 +1,30 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
 
-
+// GET /api/set-token-cookie
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
 
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
 });
+
+// GET /api/restore-user
+
+router.use(restoreUser);
+
+
+
 
 
 
