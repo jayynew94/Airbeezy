ea0a64f23fdfa469494e8ab89afd3c7311887a08
Jeremy New <93681678+jayynew94@users.noreply.github.com>
2022-08-30 13:16:56 -0500

    auth routes completed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,7 @@ const { restoreUser } = require("../../utils/auth.js");
 // GET /api/set-token-cookie
 const { setTokenCookie,requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
       where: {
@@ -17,19 +18,14 @@ router.get('/set-token-cookie', async (_req, res) => {
 });
 
 
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
+
+router.use(restoreUser);
 
 router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
 
-router.post("/test", (req, res) => {
-  res.json({ requestBody: req.body });
-});
 
-router.use(restoreUser);
 
 
 
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,9 +1,10 @@
 // backend/routes/api/session.js
 const express = require("express");
-const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { setTokenCookie, requireAuth} = require("../../utils/auth");
 const { User } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
+
 const router = express.Router();
 
 
@@ -21,29 +22,30 @@ const validateLogin = [
 ];
 
 // Log in
-router.post(
-  '/',
-  validateLogin,
-  async (req, res, next) => {
-    const { credential, password } = req.body;
+router.post("/", validateLogin, async (req, res, next) => {
+  const { credential, password } = req.body;
 
-    const user = await User.login({ credential, password });
+  const user = await User.login({ credential, password });
 
-    if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
-    }
-
-    await setTokenCookie(res, user);
-
-    return res.json({
-      user
-    });
+  if (!user) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = ["The provided credentials were invalid."];
+    return next(err);
   }
-);
+
+  const token = setTokenCookie(res, user);
+  const currUser = user.toJSON();
+  return res.json({
+    id: currUser.id,
+    firstName: currUser.firstName,
+    lastName: currUser.lastName,
+    email: currUser.email,
+    username: currUser.username,
+    token,
+  });
+});
 
 // Log out
 router.delete(
@@ -54,19 +56,19 @@ router.delete(
   }
 );
 
-// Restore session user
-router.get(
-  '/',
-  restoreUser,
-  (req, res) => {
-    const { user } = req;
-    if (user) {
-      return res.json({
-        user: user.toSafeObject()
-      });
-    } else return res.json({});
-  }
-);
+//Get current User
+router.get("/", requireAuth, async (req, res) => {
+  const user = req.user;
+  console.log();
+  res.json({
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    username: user.username,
+  });
+});
+
 
 
 
