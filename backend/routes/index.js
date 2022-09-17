8d43f4333f6c4c1fed532f39fd65a93404cc856a
Jeremy New <93681678+jayynew94@users.noreply.github.com>
2022-09-17 12:35:30 -0500

    heroku deployment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 // backend/routes/index.js
 const express = require("express");
 const router = express.Router();
+const apiRouter = require('./api')
 
 
 // Add a XSRF-TOKEN cookie
@@ -24,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -35,20 +36,18 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
 
-
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
-
 
 module.exports = router;
